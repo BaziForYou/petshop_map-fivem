6a7b7c40195c729248bebd1bb2e20b74aeaca4cd
Mohammad amin Jafarian <amin.jafarian82@gmail.com>
2021-02-08 18:57:02 +0330

    Add files via upload

---
+++ petshop_map/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+game 'gta5'
+
+name "petshop_map"
+description 'The better version of petshop_map and moved.'
+author "moved and re-edit by BaziForYou#9907 original map made by K4MB1#4372"
+version '1.1'
+
+
+this_is_a_map 'yes'
